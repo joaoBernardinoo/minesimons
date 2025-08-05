95faf6f7a3d265e7fe621b650e3e477c9936e762
joaoBernardinoo <joaobernardino@ufba.br>
2025-08-05 17:10:33 -0300

    .

--- prism.cmd
+++ prism.cmd
@@ -1,24 +1,16 @@
-@echo off
-setlocal
+echo off
 
-:: Cross-platform launcher script for MineSimons Repository Update
-:: Detects OS and runs the appropriate script
+goto(){
+# Linux code here
+uname -o
+./prism.sh
+}
 
-echo MineSimons Cross-Platform Launcher
-echo Detecting operating system...
+goto $@
+exit
 
-:: Get the directory where this script is located
-set "SCRIPT_DIR=%~dp0"
-
-:: Windows detected - run prism.bat
-echo Windows detected - launching prism.bat
-
-set "BAT_PATH=%SCRIPT_DIR%prism.bat"
-
-if exist "%BAT_PATH%" (
-    call "%BAT_PATH%"
-) else (
-    echo Error: prism.bat not found in %SCRIPT_DIR%
-    pause
-    exit /b 1
-)
+:(){
+rem Windows script here
+echo %OS%
+./prism.bat
+exit
